578ef086c80d2f20fe58c8660192c2309bb51d81
Rose Davidson <rhd@google.com>
2018-06-06 11:41:35 -0700

    Bump subminor version (1.6.0 -> 1.6.1)
    Rationale: The 'report API version' commit broke things.

--- endpoints_management/__init__.py
+++ endpoints_management/__init__.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import
 
 from . import auth, config, control, gen
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 
 USER_AGENT = u'ESP'
 SERVICE_AGENT = u'EF_PYTHON/' + __version__
